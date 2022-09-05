fee5a42e011dfb74fe56c45442a98f7ab61e7c36
sarahagh <saraa.hagh@gmail.com>
2022-09-05 19:38:13 +0430

    create weather section

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import InfoCard from "./components/infoCard/infoCard";
 import ChartSection from "./components/chartSection/chartSection";
 import Projects from "./components/projects/projects";
 import { Grid } from "@mui/material";
 import OrdersOverview from "./components/ordersOverview/ordersOverview";
+import axios from "axios";
 
 export default function Dashboard() {
+ 
+
   return (
     <div>
       <InfoCard />
       <ChartSection />
+
+
 
       <div>
         <Grid container spacing={8}>
